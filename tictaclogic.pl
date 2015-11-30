8ca612af711175d59d7952103431bc26bb0fe8a1
gtugablue <gtugablue@gmail.com>
2015-11-30 18:22:54 +0000

    Board creation.

--- tictaclogic.pl
+++ tictaclogic.pl
@@ -1,2 +1,9 @@
 :- use_module(library(clpfd)).
 
+tictaclogic(B, S) :- board(B, S, S).
+
+board([], _, 0) :- !.
+board([H | T], Width, Height) :-
+        length(H, Width),
+        Height1 is Height - 1,
+        board(T, Width, Height1).
