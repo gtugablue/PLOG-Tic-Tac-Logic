06f5df9ad1a0d9ff869ed5f7f280f791ff047caf
gtugablue <gtugablue@gmail.com>
2015-11-30 18:46:42 +0000

    ...

--- tictaclogic.pl
+++ tictaclogic.pl
@@ -1,9 +1,23 @@
 :- use_module(library(clpfd)).
 
-tictaclogic(B, S) :- board(B, S, S).
+tictaclogic(B, S) :-
+        board(B, S, S),
+        list_board_vars(B, L),
+        domain(L, 1, 2),
+        labeling([], L).
 
 board([], _, 0) :- !.
 board([H | T], Width, Height) :-
         length(H, Width),
         Height1 is Height - 1,
         board(T, Width, Height1).
+
+test_board(B, 3) :-
+        B = [[0, 0, 0],
+             [0, 0, 0],
+             [0, 0, 0]].
+
+list_board_vars([], []).
+list_board_vars([Bh | Bt], L) :-
+        append(L1, Bh, L),
+        list_board_vars(Bt, L1).
