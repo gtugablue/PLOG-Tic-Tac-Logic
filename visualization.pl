9c815e567806b9b9a8a2234bd30c2a820c943a79
aslmbc13 <asl_mbc_13@hotmail.com>
2015-11-30 18:48:43 +0000

    Visualization

--- visualization.pl
+++ visualization.pl
@@ -4,11 +4,35 @@ piece_to_ascii(0, ' ').
 piece_to_ascii(1, 'X').
 piece_to_ascii(2, 'O').
 
-print_board([H | T]) :-
-        
+print_board([Line]) :-
+        print_line(Line),
+        length(Line, Size),
+        print_separating_line(Size).
+print_board([Line|Rest]) :-
+        print_line(Line),
+        print_board(Rest).
+print_board([]).
 
+print_line(Line) :-
+        length(Line, Size),
+        print_separating_line(Size),
+        print_line_cells(Line).
 
+print_separating_line(Size) :-
+        Size > 0,
+        print_separating_line_aux,
+        S1 is Size - 1,
+        print_separating_line(S1).
+print_separating_line(0) :- write('-'), nl.
 
+print_separating_line_aux :- write('----').
 
+print_line_cells([Cell|Rest]) :-
+        write('|'),
+        print_cell(Cell),
+        print_line_cells(Rest).
+print_line_cells([]) :- write('|'), nl.
 
-
+print_cell(Cell) :-
+        piece_to_ascii(Cell, P),
+        write(' '), write(P), write(' ').
