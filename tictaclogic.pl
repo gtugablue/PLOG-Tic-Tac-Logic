fe59d2ff68628c5f9b77f294c577470085fd797b
gtugablue <gtugablue@gmail.com>
2015-12-02 09:24:30 +0000

    PINTOU!

--- tictaclogic.pl
+++ tictaclogic.pl
@@ -15,7 +15,8 @@ tictaclogic(B, S) :-
         M = 0, % Size must be even
         cross(X),
         circle(O),
-        board(B, S, S),
+        %board(B, S, S),
+        test_board(B, 6),
         transpose(B, B1),
         list_board_vars(B, L),
         domain(L, 1, 2),
@@ -24,13 +25,16 @@ tictaclogic(B, S) :-
         S2 is S * S,
         N2 is S2 div 2,
         global_cardinality(L, [X-N2, O-N2]),
+        no_more_than_two_consecutive(B),
+        no_more_than_two_consecutive(B1),
         same_number(B, N, X, O),
         same_number(B1, N, X, O),
-        all_different_lists(B),
-        all_different_lists(B1),
+        %all_different_lists(B),
+        %all_different_lists(B1),
         % Labeling
         labeling([], L),
-        fd_statistics('backtracks', V), write(V).
+        fd_statistics('backtracks', V), write(V), nl,
+        print_board(B).
 
 same_number([], _, _, _).
 same_number([H | T], N, X, O) :-
@@ -57,18 +61,36 @@ board([H | T], Width, Height) :-
         Height1 is Height - 1,
         board(T, Width, Height1).
 
-test_board(B, 3) :-
-        B = [[0, 0, 0],
-             [0, 0, 0],
-             [0, 0, 0]].
-
-teste(1).
-teste(2).
+test_board(B, 6) :-
+        cross(X),
+        circle(O),
+        B = [[_, _, X, _, _, _],
+             [_, _, X, _, _, _],
+             [X, _, _, _, _, X],
+             [_, _, O, _, _, _],
+             [_, X, _, _, _, X],
+             [O, _, _, _, O, _]].
 
 list_board_vars([], []).
 list_board_vars([Bh | Bt], L) :-
         append(Bh, L1, L),
         list_board_vars(Bt, L1).
+
+no_more_than_two_consecutive([]).
+no_more_than_two_consecutive([H | T]) :-
+        no_more_than_two_consecutive_aux(H),
+        no_more_than_two_consecutive(T).
+no_more_than_two_consecutive_aux([_]).
+no_more_than_two_consecutive_aux([H, H1 | T]) :-
+        H #\= H1,
+        no_more_than_two_consecutive_aux([H1 | T]).
+no_more_than_two_consecutive_aux([H, H1 | T]) :-
+        H #= H1,
+        no_more_than_two_consecutive_aux2([H1 | T]).
+no_more_than_two_consecutive_aux2([_]).
+no_more_than_two_consecutive_aux2([H, H1 | T]) :-
+        H #\= H1,
+        no_more_than_two_consecutive_aux([H1 | T]).
 
 %sel([H | T], H, T).
 %sel(Vars, Selected, Rest) :- random_select(Selected, Vars, Rest), var(Selected).
